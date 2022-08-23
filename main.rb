3be5f21c8959ac4e0ca59958d00d0a382cb6db0f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-08-23 13:16:45 -0500

    lecture added

--- main.rb
+++ main.rb
@@ -10,6 +10,7 @@ SELECT * FROM flats WHERE id = 43;
 
 https://www.airbnb.com/flats/search?location=Paris&price_min=50&price_max=100&s=1
 
+# THE TRICK (column table_name filters)
 SELECT * FROM flats
 SELECT * FROM flats
 WHERE location = 'Paris'
@@ -19,17 +20,17 @@ AND status = 'available'
 ORDER BY price ASC;
 
 # Usuing SQLite3
-# READ = (SELECT ... FROM)
+# READ = (SELECT ... FROM table_name)
 # SELECT * FROM doctors;
 # SELECT * FROM doctors WHERE id = 1;
 # SELECT name FROM doctors WHERE name LIKE '%John%';
 
 
-# CREATE = (INSERT INTO ... VALUES)
+# CREATE = (INSERT INTO table_name VALUES ...)
 # INSERT INTO doctors (name, age, specialty) VALUES ('Ashley Iwamoto Lira', 45, 'Pediatrics');
 
 
-# UPDATE = (UPDATE ... SET ... WHERE)
+# UPDATE = (UPDATE table_name SET ... WHERE)
 # UPDATE doctors SET name = 'Javier Pedro Zúñiga' WHERE id = 1;
 
 # DELETE = (DELETE FROM ... WHERE)
